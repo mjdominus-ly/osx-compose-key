ea72868e1b56379b8915a851404c45b79042459a
Mark Dominus <mjd@leapyear.io>
2022-07-01 09:37:25 -0400

    Update installer script
    * DOES NOT YET HANDLE OS 12.
    
    * It formerly assumed that it was running on MacOS 10.
      It had different behavior for >= 10.4, but
      11.1 would be treated like 10.1.
    
    * Attempt to not overwrite already-installed files
    
    * Update calls to `brew cask`, now `brew ... --cask`
    
    * Replace hardwired filenames with variables

--- install.sh
+++ install.sh
@@ -1,42 +1,92 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
-OSX_VERSION="$(sw_vers -productVersion | sed -E "s/^[0-9]+\.([0-9]+)/\\1/")"
+set -o pipefail
+
+OSX_VERSION="$(sw_vers -productVersion)"
+MINOR_VERSION=${OSX_VERSION#*.}
+MAJOR_VERSION=${OSX_VERSION%.*}
+
+KARABINER_CONF="$HOME/.karabiner.d/configuration/karabiner.json"
+KARABINER_PRIV="$HOME/Library/Application Support/Karabiner/private.xml"
+DEFAULT_BINDING="$HOME/Library/KeyBindings/DefaultKeyBinding.dict"
+
+ls -l "$KARABINER_CONF" "$KARABINER_PRIV" "$DEFAULT_BINDING"
+
+function error () {
+    echo "$@" >&2
+}
+
+function verify_does_not_exist () {
+    ok=true
+    for file in "$@"; do
+	if [[ -e "$file" ]] ; then
+	    ls -l "$file"
+	    error "warning: '$file' already exists; will not overwrite"
+	    ok=false
+	fi
+    done
+
+    if ! $ok; then
+	exit 1
+    fi
+}
 
 # Install Karabiner if it isn't installed, but homebrew and cask are.
 if [[ "$(type -P brew)" && "$(brew tap | awk '/cask/')" ]]; then
-  if [[ ! "$(brew cask list 2>/dev/null | grep karabiner)" ]]; then
+  if [[ ! "$(brew list --cask 2>/dev/null | grep karabiner)" ]]; then
     echo "Installing Karabiner..."
-	if [[ $OSX_VERSION -ge 12 ]]; then
-	  brew cask install karabiner-elements
-	else
-	  brew cask install karabiner
-	fi
+    if [[ $MAJOR_VERSION -ge 11 || $MAJOR_VERSION -eq 10 && $MINOR_VERSION -ge 12 ]]; then
+	brew install --cask karabiner-elements
+    else
+	brew install --cask karabiner
+    fi
   fi
   echo "Karabiner installed."
+  verify_does_not_exist "$KARABINER_CONF" "$KARABINER_PRIV"
 else
-  echo "Homebrew and Homebrew Cask not detected. Not installing Karabiner."
+  echo "Homebrew and Homebrew Cask not detected; can't install Karabiner."
   echo "If you intended for this script to install Karabiner, install"
   echo "Homebrew and Homebrew Cask:"
   echo
   echo "http://brew.sh/"
   echo "http://caskroom.io/"
   echo
+  exit 1
 fi
 
 # Copy Karabiner settings.
 echo "Copying Karabiner settings..."
-if [[ $OSX_VERSION -ge 12 ]]; then
-  mkdir -p ~/.karabiner.d/configuration
-  cp karabiner.json ~/.karabiner.d/configuration/karabiner.json
-else
-  mkdir -p ~/Library/Application\ Support/Karabiner
-  cp private.xml ~/Library/Application\ Support/Karabiner/private.xml
-fi
+case $MAJOR_VERSION in
+     10 )
+	 if [[ $MINOR_VERSION -ge 12 ]]; then
+	     mkdir -p "$(dirname "$KARABINER_CONF")"
+	     cp karabiner.json "$KARABINER_CONF"
+	 else
+	     mkdir -p "$(dirname "$KARABINER_PRIV")"
+	     cp private.xml "$KARABINER_PRIV"
+	 fi
+	 ;;
+     12)   # MJD fill this in
+	 error "Installer may not support OSX version $OSX_VERSION; consult README.md"
+	 error "MJD should fix this"
+	 exit 1
+	 mkdir -p "$(dirname "$KARABINER_PRIV")"
+	 cp private.xml "$KARABINER_PRIV"
+	 ;;
+     * )
+	 error "Installer may not support OSX version $OSX_VERSION; consult README.md"
+	 exit 1
+esac
 
 # Copy DefaultKeyBinding.dict
 mkdir -p ~/Library/KeyBindings
-echo "Copying DefaultKeyBinding.dict..."
-cp DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+if [[ -e "$DEFAULT_BINDING" ]]; then
+    error "'$DEFAULT_BINDING' already exists; will not overwrite"
+else
+    echo "Copying DefaultKeyBinding.dict..."
+    cp DefaultKeyBinding.dict "$DEFAULT_BINDING"
+fi
 
 echo
 echo "Done."
+exit 0
